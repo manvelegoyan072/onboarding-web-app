e759e9e971db254660d43214b760fa5b98374b2e
Artem Pasyugin <APasyugin@alfabank.ru>
2022-03-07 23:20:40 +0300

    server.js ok

--- ui/src/index.js
+++ ui/src/index.js
@@ -15,13 +15,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-const startGracefulShutdown = () => {
-  console.log('Starting shutdown of express...');
-  server.close(function () {
-    console.log('Express shut down.');
-  });
-}
-
-process.on('SIGTERM', startGracefulShutdown);
-process.on('SIGINT', startGracefulShutdown);
